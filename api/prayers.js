2b9fd8b7f7d4c8a8ba93ea28511d355a2f11ef4d
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-12 16:29:02 -0500

    feat: add get/next route and update the seed file

--- api/prayers.js
+++ api/prayers.js
@@ -6,3 +6,17 @@ module.exports = router
 router.get('/', (req, res, next) => {
   res.json('Test Get Route for prayers')
 })
+
+router.get('/next', (req, res, next) => {
+  Prayer.findOne({
+    order: [['views']]
+  })
+  .then(prayer => {
+    let views = prayer.views
+    return prayer.update({
+      views: views + 1
+    })
+  })
+  .then(prayer => res.send(prayer))
+  .catch(console.error)
+})
--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 const db = require('./db')
-const { User, Item, Product, Review, Categories } = require('./db/models')
+const { User, Prayer } = require('./db/models')
 
 async function seed () {
   await db.sync({force: true})
@@ -10,7 +10,22 @@ async function seed () {
     User.create({email: 'murphy@email.com', password: '123'})
   ])
 
-  console.log(`seeded ${users.length} users`)
+  const prayers = await Promise.all([
+    Prayer.create({
+      subject: 'My Mother',
+      body: 'My mom is sick. We found out a few days ago, and we need to wait a few more days before they can do further testing. Please pray for her good health.',
+      views: 0,
+      closed: false
+    }),
+    Prayer.create({
+      subject: 'I suffer from anxiety',
+      body: 'I have been struggling with anxiety attacks, and they seem to be getting worse. I want to be able to lead a normal life. Please pray for me.',
+      views: 0,
+      closed: false
+    }),
+  ])
+
+  console.log(`seeded ${users.length} users and ${prayers.length} prayers`)
   console.log(`seeded successfully`)
 }
 
