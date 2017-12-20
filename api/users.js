0bb1b9deb4ea3249bc995a989aa2662ea8a48d24
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-20 14:08:39 -0600

    feat: add post route for sessions on login

--- api/users.js
+++ api/users.js
@@ -21,3 +21,21 @@ router.post('/', (req, res, next) => {
   }))
   .catch(console.error)
 })
+
+router.post('/sessions', (req, res, next) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send('You must send an email and password')
+  }
+  User.findOne({where: {email: req.body.email}})
+  .then(foundUser => {
+    if (!foundUser || !foundUser.correctPassword(req.body.password)) {
+      res.status(401).send('The email or password is incorrect')
+    } else {
+      res.status(201).send({
+        userId: foundUser.id,
+        jwToken: createToken(foundUser)
+      })
+    }
+  })
+  .catch(console.error)
+})
