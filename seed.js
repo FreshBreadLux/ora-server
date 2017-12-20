6b65ac3706a1995d97047f248685ec7eda3ec9e0
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-20 17:15:39 -0600

    feat: add more robust seeding

--- seed.js
+++ seed.js
@@ -15,13 +15,43 @@ async function seed () {
       subject: 'My Mother',
       body: 'My mom is sick. We found out a few days ago, and we need to wait a few more days before they can do further testing. Please pray for her good health.',
       views: 0,
-      closed: false
+      closed: false,
+      userId: 1
+    }),
+    Prayer.create({
+      subject: 'Big Test',
+      body: 'I have been studying for my medical exams for almost a year now. For better or worse, the results of these exams will determine much of my future. Please pray that I remember what I have studied, that I remain calm, and that the will of God is carried out.',
+      views: 0,
+      closed: false,
+      userId: 1
+    }),
+    Prayer.create({
+      subject: 'Mission Trip',
+      body: 'My young adult group is going on a mission trip to Mexico to build houses for families in need. Please pray that we have a safe and successful trip, and that everyone involved grows in virtue.',
+      views: 0,
+      closed: false,
+      userId: 1
     }),
     Prayer.create({
       subject: 'I suffer from anxiety',
       body: 'I have been struggling with anxiety attacks, and they seem to be getting worse. I want to be able to lead a normal life. Please pray for me.',
       views: 0,
-      closed: false
+      closed: false,
+      userId: 2,
+    }),
+    Prayer.create({
+      subject: 'My Friend is Pregnant',
+      body: 'My friend recently found out that she is pregnant, and has been considering an abortion. She is very overwhelmed, but she needs to understand that an abortion is not an option. Please pray that she will open her heart to God and to her baby.',
+      views: 0,
+      closed: false,
+      userId: 2,
+    }),
+    Prayer.create({
+      subject: 'The Conversion of My Husband',
+      body: 'I have been trying to convince my husband to come into full communion with the Catholic Church for years now. He has seemed interested for a while, but I am getting worn out and I am starting to wonder if it will ever go anywhere. Please pray that God reaches out to him and convinces him to come home.',
+      views: 0,
+      closed: false,
+      userId: 2,
     }),
   ])
 
