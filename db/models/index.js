7dadc39eb7ef8f56747f042c676711c941d182f4
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-20 17:32:36 -0600

    feat: add Following association

--- db/models/index.js
+++ db/models/index.js
@@ -6,6 +6,8 @@ User.hasMany(Prayer)
 Prayer.belongsTo(User)
 User.belongsToMany(Prayer, { through: 'Flagged' })
 Prayer.belongsToMany(User, { through: 'Flagged' })
+User.belongsToMany(Prayer, { through: 'Following' })
+Prayer.belongsToMany(User, { through: 'Following' })
 
 module.exports = {
   User,
