96a3cf9b7b88c2ea21d5bcece63802e2393db454
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-20 19:55:09 -0600

    chore: remove test route from prayers.js

--- api/prayers.js
+++ api/prayers.js
@@ -3,10 +3,6 @@ const Prayer = require('../db/models/prayer')
 
 module.exports = router
 
-router.get('/', (req, res, next) => {
-  res.json('Test Get Route for prayers')
-})
-
 router.get('/next', (req, res, next) => {
   Prayer.findOne({
     order: [['views']]
