f524942e27afeaec062bbec28a2e36ab30759ffa
Bobby Lux <robert.michael.lux@gmail.com>
2017-12-22 19:49:59 -0600

    feat: change associations with follow and prayer and user

--- db/models/index.js
+++ db/models/index.js
@@ -5,8 +5,8 @@ const Follow = require('./follow')
 /** Associations **/
 User.hasMany(Prayer)
 Prayer.belongsTo(User)
-Follow.hasOne(User)
-Follow.hasOne(Prayer)
+User.belongsToMany(Follow)
+Prayer.belongsToMany(Follow)
 
 module.exports = {
   User,
