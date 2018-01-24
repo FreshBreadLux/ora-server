3130fa014c31c262bf84671c772fae02063a1ba3
Bobby Lux <robert.michael.lux@gmail.com>
2018-01-24 17:25:39 -0500

    chore: add console.logs to scheduler script

--- scheduler.js
+++ scheduler.js
@@ -4,13 +4,18 @@ const Prayer = require('./db/models/prayer')
 const clearViews = () => {
   db.sync()
   .then(() => {
+    console.log('db synced!')
     Prayer.update(
       { dailyViews: 0 },
       { where: {} })
     .then(() => console.log('dailyViews reset to zero'))
     .catch(console.error)
   })
-  .then(() => db.close())
+  .then(() => {
+    console.log('closing db')
+    db.close()
+    console.log('db closed!')
+  })
 }
 
 clearViews()
