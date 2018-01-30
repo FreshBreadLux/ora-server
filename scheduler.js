b36afa7686e7fdf52570ac2ff9e1dcbaa497e2d2
Bobby Lux <robert.michael.lux@gmail.com>
2018-01-30 17:31:05 -0500

    feat: add consecutiveDay increment to scheduler function

--- scheduler.js
+++ scheduler.js
@@ -5,16 +5,25 @@ const updateConsecutive = async () => {
   try {
     await db.sync()
     console.log('db synced!')
+
     await User.update(
       { consecutiveDays: 0 },
       { where: { prayedToday: false } }
     )
     console.log('consecutiveDays reset to zero for users who have not prayed today')
+
+    await User.increment(
+      'consecutiveDays',
+      { where: { prayedToday: true } }
+    )
+    console.log('consecutiveDays incremented for users who have prayed today')
+
     await User.update(
       { prayedToday: false },
       { where: {} }
     )
     console.log('prayedToday reset to false for all users')
+
     console.log('closing db')
     db.close()
     console.log('db closed!')
