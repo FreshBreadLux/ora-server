677174d2c2dee70f219720f31cdc4557366e797f
Bobby Lux <robert.michael.lux@gmail.com>
2018-02-02 17:39:23 -0500

    feat: receive a message from the client on new-view channel

--- socketio.js
+++ socketio.js
@@ -2,6 +2,10 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`connected socket: ${socket.id}`)
 
+    socket.on('new-view', message => {
+      console.log('message: ', message)
+    })
+
     socket.on('disconnect', () => {
       console.log(`disconnected socket: ${socket.id}`)
     })
