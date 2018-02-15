1030bf4d216b16c79b88cbf09b7ffea9c13bfb73
Bobby Lux <robert.michael.lux@gmail.com>
2018-02-15 13:16:57 -0500

    chore: config alterations

--- api/flags.js
+++ api/flags.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
 const Flag = require('../db/models/flag')
+const nodemailer = require('nodemailer')
+const { NODEMAILER_USER, NODEMAILER_PASS } = require('../secrets')
 
 module.exports = router
 
+const smtpTransport = nodemailer.createTransport('SMTP', {
+  service: 'Gmail',
+  auth: {
+    user: NODEMAILER_USER,
+    pass: NODEMAILER_PASS
+  }
+})
+
 router.post('/', (req, res, next) => {
+  console.log('Blah')
   Flag.create(req.body)
   .then(newFlag => res.status(201).send(newFlag))
   .catch(console.error)
