3a46597452a2fdca27620675ae2b644666766ac4
Bobby Lux <robert.michael.lux@gmail.com>
2018-02-15 13:48:00 -0500

    chore: add more descriptive message to email on new flag

--- api/flags.js
+++ api/flags.js
@@ -5,27 +5,27 @@ const { NODEMAILER_USER, NODEMAILER_PASS } = require('../secrets')
 
 module.exports = router
 
-const smtpTransport = nodemailer.createTransport('SMTP', {
-  service: 'Gmail',
+const smtpTransport = nodemailer.createTransport({
+  service: 'gmail',
   auth: {
     user: NODEMAILER_USER,
     pass: NODEMAILER_PASS
   }
 })
 
-const mailOptions = {
-  from: NODEMAILER_USER,
-  to: NODEMAILER_USER,
-  subject: 'New Flag in Ora',
-  text: 'Someone has flagged a prayer in the Ora prayer network. Check the database for details and take appropriate action.'
-}
-
 router.post('/', (req, res, next) => {
   Flag.create(req.body)
-  .then(newFlag => res.status(201).send(newFlag))
-  .then(() => smtpTransport.sendMail(mailOptions, (err, resp) => {
-    if (err) console.error(err)
-    else console.log('Flag email sent: ', resp.message)
-  }))
+  .then(newFlag => {
+    res.status(201).send(newFlag)
+    smtpTransport.sendMail({
+      from: NODEMAILER_USER,
+      to: NODEMAILER_USER,
+      subject: 'New Flag in Ora',
+      text: `User ${newFlag.flaggerId} just flagged prayer ${newFlag.flaggedId}. Check the data base for more details, and take appropriate action.`
+    }, (err, info) => {
+      if (err) console.error(err)
+      else console.log('Flag email sent: ', info.response)
+    })
+  })
   .catch(console.error)
 })
