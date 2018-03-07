ffb6cd63f230d2a47b450590ea4eaebeff67ba06
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-07 17:10:23 -0500

    feat: add Follow model with newUpdates field; plug model into associations

--- db/models/index.js
+++ db/models/index.js
@@ -1,14 +1,15 @@
 const User = require('./user')
 const Prayer = require('./prayer')
 const Flag = require('./flag')
+const Follow = require('./follow')
 const FlagReason = require('./flagreason')
 const Update = require('./update')
 
 /** Associations **/
 User.hasMany(Prayer)
 Prayer.belongsTo(User)
-User.belongsToMany(Prayer, { through: 'follow', as: 'followed', foreignKey: 'followerId' })
-Prayer.belongsToMany(User, { through: 'follow', as: 'follower', foreignKey: 'followedId' })
+User.belongsToMany(Prayer, { through: Follow, as: 'followed', foreignKey: 'followerId' })
+Prayer.belongsToMany(User, { through: Follow, as: 'follower', foreignKey: 'followedId' })
 User.belongsToMany(Prayer, { through: Flag, as: 'flagged', foreignKey: 'flaggerId' })
 Prayer.belongsToMany(User, { through: Flag, as: 'flagger', foreignKey: 'flaggedId' })
 User.belongsToMany(Prayer, { through: 'view', as: 'viewed', foreignKey: 'viewerId' })
