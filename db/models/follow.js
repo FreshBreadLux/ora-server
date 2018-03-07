73086b60a695b2204aca63d2b25fca56af4f62dc
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-07 17:18:49 -0500

    chore: update follow model

--- db/models/follow.js
+++ db/models/follow.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Follow = db.define('follow', {
+  // Unused as of version 1.0.1; may be used in later versions
   newUpdates: {
     type: Sequelize.INTEGER,
     allowNull: false,
