61616e73827e19cfac79c3155950fb0b1b5c7165
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-21 11:52:44 -0400

    feat: add support page to api routes

---
+++ api/support.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+const nodemailer = require('nodemailer')
+
+module.exports = router
+
+const smtpTransport = nodemailer.createTransport({
+  host: 'smtp-relay.sendinblue.com',
+  port: 587,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS
+  }
+})
+
+router.post('/', (req, res, next) => {
+  smtpTransport.sendMail({
+    from: req.body.email,
+    to: process.env.NODEMAILER_USER,
+    subject: `Website Support Form: ${req.body.subject}`,
+    text: req.body.body
+  }, (err, info) => {
+    if (err) {
+      console.error(err)
+      res.status(300).send('There was an error sending your message')
+    }
+    else {
+      console.log('Message sent: ', info)
+      res.send('Thank you! Your message has been sent')
+    }
+  })
+})
