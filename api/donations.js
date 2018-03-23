1c93d28c98f4c9967314563bb10e5247bb1ba5a9
Bobby Lux <robert.michael.lux@gmail.com>
2018-03-23 12:59:02 -0400

    feat: add post route to donations.js

--- api/donations.js
+++ api/donations.js
@@ -1,7 +1,19 @@
 const router = require('express').Router()
+const stripe = require('stripe')('sk_test_LR90mKCpaBvsj7u0D8Zwx6ET')
 
 module.exports = router
 
 router.post('/oneTime', (req, res, next) => {
   console.log('req.body: ', req.body)
+  const token = req.body.token
+  console.log('token received; ID: ', token.id)
+  stripe.charges.create({
+    amount: 1000,
+    currency: 'usd',
+    description: 'One time donation',
+    source: token.id,
+  }, (err, charge) => {
+    if (err) console.error(err)
+    else res.send(charge)
+  })
 })
