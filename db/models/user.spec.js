5ac06b07b6be8ce37cc8f6137bcbb700a4892856
Bobby Lux <robert.michael.lux@gmail.com>
2018-05-02 15:19:11 -0400

    feat: add user.spec.js

---
+++ db/models/user.spec.js
@@ -0,0 +1,31 @@
+const { expect } = require('chai')
+const db = require('../index')
+const User = db.models.user
+const Prayer = db.models.prayer
+
+describe('User model', () => {
+  before('Await database sync', () => db.sync({force: true}))
+  afterEach('Clear the tables', () => db.truncate({ cascade: true }))
+
+  let user
+  beforeEach('Build a user', () => {
+    user = User.build({
+      email: 'test@email.com',
+      password: '123'
+    })
+  })
+
+  it('Can save with email and password', async () => {
+    await user.save()
+  })
+
+  it('Requires an email', async () => {
+    delete user.dataValues.email
+    try {
+      await user.save()
+      throw Error('We should not have been able to save the user without an email')
+    } catch (error) {
+      expect(error.message).to.match(/user.email cannot be null/)
+    }
+  })
+})
