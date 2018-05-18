853f1238a1a66276b3f4b9a2af21f0e463c2a5a3
Bobby Lux <robert.michael.lux@gmail.com>
2018-05-18 15:37:30 -0400

    feat: add find logic back to findOrCreate in plansLogicalModel, with different plan IDs

--- utils/plansLogicalModel.js
+++ utils/plansLogicalModel.js
@@ -2,6 +2,12 @@ const stripe = require('stripe')(process.env.STRIPE_API_KEY)
 
 class Plan {
   static findOrCreate(amount) {
+    if (amount === 5000) return stripe.plans.retrieve(process.env.PLAN_50_ID)
+    else if (amount === 7500) return stripe.plans.retrieve(process.env.PLAN_75_ID)
+    else if (amount === 10000) return stripe.plans.retrieve(process.env.PLAN_100_ID)
+    else if (amount === 15000) return stripe.plans.retrieve(process.env.PLAN_150_ID)
+    else if (amount === 20000) return stripe.plans.retrieve(process.env.PLAN_200_ID)
+    else if (amount === 30000) return stripe.plans.retrieve(process.env.PLAN_300_ID)
     return stripe.plans.create({
       amount,
       currency: 'usd',
