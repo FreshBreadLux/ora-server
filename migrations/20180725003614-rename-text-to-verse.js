07c95cf776b144992c1d6b0b5fd2973466474f4f
Bobby Lux <robert.michael.lux@gmail.com>
2018-07-24 20:36:27 -0400

    feat: add migration file for rename-text-to-verse

--- migrations/20180725003614-rename-text-to-verse.js
+++ migrations/20180725003614-rename-text-to-verse.js
@@ -9,16 +9,6 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-    return [
-      queryInterface.addColumn('reflections', 'fullText', {
-        type: Sequelize.TEXT,
-        allowNull: false
-      }),
-      queryInterface.addColumn('reflections', 'source', {
-        type: Sequelize.STRING,
-        allowNull: false
-      })
-    ]
   },
 
   down: (queryInterface, Sequelize) => {
@@ -29,9 +19,5 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-    return [
-      queryInterface.removeColumn('reflections', 'fullText'),
-      queryInterface.removeColumn('reflections', 'source')
-    ]
   }
 };
