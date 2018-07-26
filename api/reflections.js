b1fa4a0516b0a0ada053a44d4e966022d993a6bf
Bobby Lux <robert.michael.lux@gmail.com>
2018-07-26 11:13:49 -0400

    feat: change text in reflections get route

--- api/reflections.js
+++ api/reflections.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
   Reflection.findOne({ where: { date: req.query.date } })
   .then(dailyReflection => {
     if (dailyReflection) return res.send(dailyReflection)
-    else return res.send({ verse: 'Our reflections are locally sourced and handpicked' })
+    else return res.send({ verse: 'Our reflections are locally sourced and handpicked fresh each day', fullText: 'We were not able to find a reflection for today' })
   })
   .catch(next)
 })
