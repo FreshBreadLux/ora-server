302c7d81b2a49cc409276d6853f2296bb31ad1c4
Bobby Lux <robert.michael.lux@gmail.com>
2018-08-07 16:57:58 -0400

    chore: change default reward image url

--- api/rewards.js
+++ api/rewards.js
@@ -7,7 +7,7 @@ router.get('/', (req, res, next) => {
   Reward.findOne({ where: { date: req.query.date } })
   .then(dailyReward => {
     if (dailyReward) return res.send(dailyReward)
-    else return res.send({ imageUrl: 'https://images.unsplash.com/photo-1504870712357-65ea720d6078?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d91ffea4f802668c524df165caf52704&auto=format&fit=crop&w=2800&q=80', iconColor: '#000' })
+    else return res.send({ imageUrl: 'https://s3.us-east-2.amazonaws.com/ora-images/church-reflection.jpg', iconColor: '#000' })
   })
   .catch(next)
 })
