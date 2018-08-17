a253b8bf05ab331c0d7e2259735d36d1da27bfa5
Bobby Lux <robert.michael.lux@gmail.com>
2018-08-17 12:12:52 -0400

    feat: include Artists when fetching the daily reward

--- api/rewards.js
+++ api/rewards.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
 const Reward = require('../db/models/reward')
+const Artist = require('../db/models/artist')
 
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  Reward.findOne({ where: { date: req.query.date } })
+  Reward.findOne({
+    where: { date: req.query.date },
+    include: [{ model: Artist }]
+  })
   .then(dailyReward => {
     if (dailyReward) return res.send(dailyReward)
-    else return res.send({ imageUrl: 'https://s3.us-east-2.amazonaws.com/ora-images/church-reflection.jpg', iconColor: '#000' })
+    else return res.send({ imageUrl: 'https://s3.us-east-2.amazonaws.com/ora-images/church-reflection.jpg', iconColor: '#fff' })
   })
   .catch(next)
 })
