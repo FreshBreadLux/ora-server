6880964666b8b824e5bb05cb838f89b3cac93f73
Bobby Lux <robert.michael.lux@gmail.com>
2018-09-25 17:49:31 -0400

    feat: add sequelize migration file for notificationsEnabled

---
+++ migrations/20180925214711-user-add-notificationsEnabled.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn('users', 'notificationsEnabled', {
+      type: Sequelize.BOOLEAN
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeColumn('users', 'notificationsEnabled')
+  }
+};
