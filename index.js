c5df1310a55716efde734b8cb817dc66c1f3e61e
Bobby Lux <robert.michael.lux@gmail.com>
2021-05-01 11:38:15 -0400

    remove bodyparse and newrelic

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-require('newrelic')
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
 const compression = require('compression')
 const passport = require('passport')
 const session = require('express-session')
@@ -38,8 +36,8 @@ const createApp = () => {
   app.use(morgan('dev'))
 
   // body parsing middleware
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
 
   // compression middleware
   app.use(compression())
