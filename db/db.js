e575e0360c70177c91115dd4559505d0e1ea8702
Bobby Lux <robert.michael.lux@gmail.com>
2021-05-03 21:24:19 -0400

    update db configs for ssl

--- db/db.js
+++ db/db.js
@@ -1,7 +1,26 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/ora-staging', {
+
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
     logging: false
   }
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/ora-staging',
+  config
 )
 module.exports = db
